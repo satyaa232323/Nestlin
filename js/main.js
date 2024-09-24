814d913b4b58824a88d766dc1b546c5a730273fc
satyaa232323 <attalasatya@gmail.com>
2024-09-24 20:17:28 +0700

    Navbar Responsif

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
-// login
-const txUsername = document.querySelector('#username');
-const txPassword = document.querySelector('#password');
-const btnSubmit = document.querySelector('#btSubmit');
+// // login
+// const txUsername = document.querySelector('#username');
+// const txPassword = document.querySelector('#password');
+// const btnSubmit = document.querySelector('#btSubmit');
+
+// const btLogin = document.getElementById('#btLogin');
+
+// btnSubmit.addEventListener('click', (e) => {
+//   e.preventDefault();
+
+//   if(txUsername.value == 'admin' && txPassword.value == 'admin'){
+//     window.location.href = "index.html";
+//   }
+// });
+// // endlogin
+
+// function login(){
+//     window.location.href = "login.html";
+ 
+// }
 
 
-btnSubmit.addEventListener('click', (e) => {
-  e.preventDefault();
+// Navbar Responsive
+const navToggle = document.getElementById('nav-toggle');
+const navbar = document.querySelector('.navbar');
 
-  if(txUsername.value == 'admin' && txPassword.value == 'admin'){
-    window.location.href = "index.html";
-  }
+// Add event listener to toggle the navbar visibility
+navToggle.addEventListener('click', () => {
+    // Toggle the 'active' class on the navbar
+    navbar.classList.toggle('active');
 });
-// endlogin
-
-// btn masuk
-
-
