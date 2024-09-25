e4e45175c94ac8c0a3465da6cdfcd269f24160c1
satyaa232323 <attalasatya@gmail.com>
2024-09-25 07:07:17 +0700

    Navbar links

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-// // login
-// const txUsername = document.querySelector('#username');
-// const txPassword = document.querySelector('#password');
-// const btnSubmit = document.querySelector('#btSubmit');
-
-// const btLogin = document.getElementById('#btLogin');
-
-// btnSubmit.addEventListener('click', (e) => {
-//   e.preventDefault();
-
-//   if(txUsername.value == 'admin' && txPassword.value == 'admin'){
-//     window.location.href = "index.html";
-//   }
-// });
-// // endlogin
-
-// function login(){
-//     window.location.href = "login.html";
- 
-// }
-
-
-// Navbar Responsive
+// / Navbar Responsive
 const navToggle = document.getElementById('nav-toggle');
 const navbar = document.querySelector('.navbar');
 
