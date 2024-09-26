b548d811e00c37e72d7da49c8593a0933c9614b8
Gessa Lutfhan Waldhani <gieshalutfian@gmail.com>
2024-09-26 23:34:39 +0700

    Login-baru

---
+++ js/login.js
@@ -0,0 +1,12 @@
+// Menambahkan event listener untuk tombol Daftar
+const btnRegister = document.querySelector("#create");
+btnRegister.addEventListener("click", () => {
+  // Mengarahkan ke halaman createakun.html saat tombol "Daftar" diklik
+  window.location.href = "createakun.html";
+});
+
+// Masuk
+const btnLogin = document.querySelector("#btLogin");
+btnLogin.addEventListener("click", () => {
+  window.location.href = "login.html";
+});
