f61c72f7a91ee018d2ebb8a38719eeaae2af3ff9
Gessa Lutfhan Waldhani <gieshalutfian@gmail.com>
2024-09-29 15:57:37 +0700

    Halo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-// / Navbar Responsive
 const navToggle = document.getElementById('nav-toggle');
 const navbar = document.querySelector('.navbar');
 
-// Add event listener to toggle the navbar visibility
 navToggle.addEventListener('click', () => {
-    // Toggle the 'active' class on the navbar
     navbar.classList.toggle('active');
 });
